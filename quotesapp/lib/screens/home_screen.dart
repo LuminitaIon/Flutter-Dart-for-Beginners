9120e669a7938b39a35e4e5162f88b47e5ab3f85
LuminitaIon <andreea_ion63@yahoo.com>
2022-10-24 16:14:04 +0300

    get and display data from api, basic UI

--- quotesapp/lib/screens/home_screen.dart
+++ quotesapp/lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:quotesapp/screens/second_screen.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -10,10 +12,17 @@ class HomeScreen extends StatelessWidget {
       child: Scaffold(
         body: Center(
           child: ElevatedButton(
-              onPressed: () { 
+              onPressed: () async {
+                var url = Uri.parse('https://api.quotable.io/random');
+                var response = await http.get(url);
+                var data = jsonDecode(response.body);
+
+                String quote = data["content"];
+                String author = data["author"];
+
                 Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => SecondScreen()));
-               },
+                  MaterialPageRoute(builder: (context) => SecondScreen(quoteText: quote, authorName: author,)));
+              },
               child: Text("Go to next Screen"),
             ),
         ),
