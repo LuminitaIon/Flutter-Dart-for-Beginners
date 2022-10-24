4cf7199813e267f2dd440e94ee864dbc0b4737a0
LuminitaIon <andreea_ion63@yahoo.com>
2022-10-24 16:45:18 +0300

    convert the statless widget to statefull widget

--- quotesapp/lib/screens/home_screen.dart
+++ quotesapp/lib/screens/home_screen.dart
@@ -1,30 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:quotesapp/screens/second_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String quote = "";
+  String author = "";
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Center(
-          child: ElevatedButton(
-              onPressed: () async {
-                var url = Uri.parse('https://api.quotable.io/random');
-                var response = await http.get(url);
-                var data = jsonDecode(response.body);
-
-                String quote = data["content"];
-                String author = data["author"];
-
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => SecondScreen(quoteText: quote, authorName: author,)));
-              },
-              child: Text("Go to next Screen"),
+        appBar: AppBar(
+        
+        ),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                quote, 
+                style: TextStyle(fontSize: 30, color: Colors.black87),
+                ),
             ),
+            Padding(
+              padding: const EdgeInsets.only(right: 8.0),
+              child: Align(
+                alignment: Alignment.bottomRight,
+                child: Text(
+                  "- $author",
+                  style: TextStyle(fontSize: 20, color: Colors.grey[700]),
+                  )
+                ),
+            ),
+            ElevatedButton(
+                onPressed: () async {
+
+                  var url = Uri.parse('https://api.quotable.io/random');
+                  var response = await http.get(url);
+                  var data = jsonDecode(response.body);
+
+                  quote = data["content"];
+                  author = data["author"];
+
+                  setState(() {});
+
+                },
+                child: Text("Get Quote!"),
+              ),
+          ],
         ),
       ),
     );
